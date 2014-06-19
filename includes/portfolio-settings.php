e9d8232cb581763d78bf790050437c738419168c
iografica <iografica@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-19 09:08:28 +0000

    included file
    git-svn-id: https://plugins.svn.wordpress.org/ig-portfolio/trunk@934970 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/portfolio-settings.php
@@ -0,0 +1,33 @@
+<?php
+
+add_action('admin_menu', 'ig_portfolio_submenu_page');
+
+function ig_portfolio_submenu_page() {
+	add_submenu_page( 'edit.php?post_type=project', 'IG Portfolio Settings', 'Settings', 'manage_options', 'ig-portfolio-submenu-page', 'ig_portfolio_submenu_page_callback' ); 
+	//call register settings function
+	add_action( 'admin_init', 'ig_portfolio_settings' );
+}
+
+function ig_portfolio_settings() {
+	//register our settings
+	register_setting( 'ig-portfolio-settings-group', 'ig_portfolio_settings' );
+}
+
+function ig_portfolio_validate($input) {
+  return array_map('wp_filter_nohtml_kses', (array)$input);
+}
+
+
+function ig_portfolio_submenu_page_callback() {
+?>
+                
+<div>
+<h2><?php echo __('IG Portfolio Settings', 'ig-portfolio') ?></h2>
+<p><?php echo __('To display projects in two or more columns and to customize the post information please upgrade to premium version.', 'ig-portfolio') ?></p>
+<h2><?php echo __('IG Portfolio Shortcodes', 'ig-portfolio') ?></h2>
+<p><?php echo __('Shortcodes are available only in the premium version.', 'ig-portfolio') ?></p>
+<a href="http://themes.iografica.it/downloads/ig-portfolio-premium/" target="_blank">
+<?php submit_button( 'Upgrade to premium version', 'primary') ?>
+</a>
+</div>	
+<?php } ?>
